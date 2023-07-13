2d4e0ab7478e3ee74610a9905e62689e2c107a03
TecnoNewsUY <139405372+TecnoNewsUY@users.noreply.github.com>
2023-07-13 15:47:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const categoria = obtenerCategoria();
-    const noticiasContainer = document.getElementById('noticias-container');
+    const categoria = '1'; // Categoría uno
 
-    // Obtener noticias de la categoría correspondiente
-    obtenerNoticias(categoria)
-        .then(noticias => {
-            // Mostrar las últimas 3 noticias
-            const ultimasNoticias = noticias.slice(-3);
-            ultimasNoticias.forEach(noticia => {
-                const noticiaHTML = generarHTMLNoticia(noticia.titulo, noticia.contenido);
-                noticiasContainer.appendChild(noticiaHTML);
+    fetch('https://raw.githubusercontent.com/TecnoNewsUY/TecnoNewsUY/master/todaslasnoticias/todaslasnoticias.json')
+        .then(response => response.json())
+        .then(data => {
+            const noticiasCategoria = data.filter(noticia => noticia.categoria.includes(categoria));
+            const noticiasMostradas = noticiasCategoria.slice(-3); // Mostrar las últimas 3 noticias
+
+            const noticiasContainer = document.getElementById('noticias-container');
+            noticiasContainer.innerHTML = ''; // Limpiar el contenedor de noticias
+
+            noticiasMostradas.forEach(noticia => {
+                const noticiaHTML = createNoticiaHTML(noticia.titulo, noticia.contenido);
+                noticiasContainer.innerHTML += noticiaHTML;
             });
         })
         .catch(error => {
@@ -17,33 +20,8 @@ window.addEventListener('DOMContentLoaded', () => {
         });
 });
 
-// Función para obtener la categoría de la página actual
-function obtenerCategoria() {
-    const url = window.location.href;
-    if (url.includes('page1.html')) {
-        return 'Noticias';
-    } else if (url.includes('page2.html')) {
-        return 'Ultima Hora';
-    } else if (url.includes('page3.html')) {
-        return 'Ofertas';
-    }
-}
-
-// Función para obtener las noticias de una categoría
-function obtenerNoticias(categoria) {
-    return fetch('https://raw.githubusercontent.com/TecnoNewsUY/TecnoNewsUY/master/todaslasnoticias/todaslasnoticias.json')
-        .then(response => response.json())
-        .then(data => {
-            const noticias = data.filter(noticia => noticia.categoria.includes(categoria));
-            return noticias;
-        });
-}
-
-// Función para generar el HTML de una noticia
-function generarHTMLNoticia(titulo, contenido) {
-    const noticiaHTML = document.createElement('div');
-    noticiaHTML.classList.add('noticia');
-    noticiaHTML.innerHTML = `
+function createNoticiaHTML(titulo, contenido) {
+    const noticiaHTML = `
         <h3>${titulo}</h3>
         <p>${contenido}</p>
     `;
