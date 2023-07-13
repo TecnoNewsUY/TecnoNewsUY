386529e3018af3645d7ff23159c623aad94107cf
TecnoNewsUY <139405372+TecnoNewsUY@users.noreply.github.com>
2023-07-13 19:23:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 window.addEventListener('DOMContentLoaded', () => {
-    // Obtener la sección de las últimas noticias
-    const latestNewsContainer = document.getElementById('latest-news');
+    // Obtener la sección del selector de noticias
+    const noticiasSelector = document.getElementById('noticias-selector');
+
+    // Obtener el contenedor del contenido de la noticia
+    const noticiaContenido = document.getElementById('noticia-contenido');
 
     // Cargar las noticias desde el archivo JSON
     fetch('https://raw.githubusercontent.com/TecnoNewsUY/TecnoNewsUY/master/todaslasnoticias/todaslasnoticias.json')
@@ -14,17 +17,16 @@ window.addEventListener('DOMContentLoaded', () => {
 
                 // Verificar si hay noticias en la categoría seleccionada
                 if (noticiasFiltradas.length > 0) {
-                    // Ordenar las noticias por fecha (de más reciente a más antigua)
-                    noticiasFiltradas.sort((a, b) => new Date(b.fecha) - new Date(a.fecha));
-
-                    // Limitar las noticias a las últimas 5
-                    const ultimasNoticias = noticiasFiltradas.slice(0, 5);
-
-                    // Mostrar las últimas noticias en el contenedor correspondiente
-                    ultimasNoticias.forEach(noticia => {
-                        const noticiaElement = crearElementoNoticia(noticia);
-                        latestNewsContainer.appendChild(noticiaElement);
+                    // Recorrer las noticias filtradas y agregar opciones al selector
+                    noticiasFiltradas.forEach((noticia, index) => {
+                        const option = document.createElement('option');
+                        option.value = index;
+                        option.textContent = noticia.titulo;
+                        noticiasSelector.appendChild(option);
                     });
+
+                    // Mostrar la primera noticia por defecto
+                    mostrarNoticia(noticiasFiltradas[0]);
                 } else {
                     mostrarError("No se encontraron noticias en la categoría seleccionada.");
                 }
@@ -36,49 +38,64 @@ window.addEventListener('DOMContentLoaded', () => {
             mostrarError("Error al cargar las noticias: " + error);
         });
 
-    // Función para crear el elemento HTML de una noticia
-    function crearElementoNoticia(noticia) {
-        const noticiaElement = document.createElement('div');
-        noticiaElement.className = 'noticia';
+    // Cambiar el contenido de la noticia al seleccionar una opción del selector
+    noticiasSelector.addEventListener('change', () => {
+        const selectedIndex = noticiasSelector.value;
+        fetch('https://raw.githubusercontent.com/TecnoNewsUY/TecnoNewsUY/master/todaslasnoticias/todaslasnoticias.json')
+            .then(response => response.json())
+            .then(data => {
+                // Verificar si el archivo JSON contiene datos
+                if (data && data.length > 0) {
+                    // Filtrar las noticias por categoría
+                    const categoriaSeleccionada = obtenerCategoriaSeleccionada();
+                    const noticiasFiltradas = data.filter(noticia => noticia.categoria === categoriaSeleccionada);
 
-        // Verificar si la noticia tiene imagen
-        if (noticia.imagen) {
-            const imagenElement = document.createElement('img');
-            imagenElement.src = noticia.imagen;
-            imagenElement.alt = noticia.titulo;
-            noticiaElement.appendChild(imagenElement);
-        }
+                    // Verificar si hay noticias en la categoría seleccionada
+                    if (noticiasFiltradas.length > 0) {
+                        const selectedNoticia = noticiasFiltradas[selectedIndex];
+                        mostrarNoticia(selectedNoticia);
+                    } else {
+                        mostrarError("No se encontraron noticias en la categoría seleccionada.");
+                    }
+                } else {
+                    mostrarError("No se encontraron noticias.");
+                }
+            })
+            .catch(error => {
+                mostrarError("Error al cargar las noticias: " + error);
+            });
+    });
 
-        const contenidoElement = document.createElement('div');
-        contenidoElement.className = 'contenido';
-
-        const tituloElement = document.createElement('h3');
-        const enlaceElement = document.createElement('a');
-        enlaceElement.href = noticia.enlace;
-        enlaceElement.textContent = noticia.titulo;
-        tituloElement.appendChild(enlaceElement);
-        contenidoElement.appendChild(tituloElement);
-
-        const cuerpoElement = document.createElement('p');
-        cuerpoElement.textContent = noticia.contenido;
-        contenidoElement.appendChild(cuerpoElement);
-
-        noticiaElement.appendChild(contenidoElement);
-
-        return noticiaElement;
+    // Función para mostrar el contenido de la noticia
+    function mostrarNoticia(noticia) {
+        const enlace = obtenerEnlaceNoticia(noticia.titulo);
+        const titulo = `<a href="${enlace}">${noticia.titulo}</a>`;
+        const imagen = noticia.imagen ? `<img src="${noticia.imagen}" alt="${noticia.titulo}">` : '';
+        const contenido = `<div class="noticia">
+                                <div class="imagen">${imagen}</div>
+                                <div class="texto">
+                                    <h3>${titulo}</h3>
+                                    <p>${noticia.contenido}</p>
+                                </div>
+                           </div>`;
+        noticiaContenido.innerHTML = contenido;
     }
 
     // Función para mostrar un mensaje de error
     function mostrarError(mensaje) {
-        const errorElement = document.createElement('p');
-        errorElement.className = 'error';
-        errorElement.textContent = mensaje;
-        latestNewsContainer.appendChild(errorElement);
+        noticiaContenido.innerHTML = `<p class="error">${mensaje}</p>`;
     }
 
     // Función para obtener la categoría seleccionada
     function obtenerCategoriaSeleccionada() {
         const urlParams = new URLSearchParams(window.location.search);
         return urlParams.get('categoria');
+    }
+
+    // Función para obtener el enlace de la noticia
+    function obtenerEnlaceNoticia(titulo) {
+        const categoria = obtenerCategoriaSeleccionada();
+        const tituloFormateado = titulo.toLowerCase().replace(/\s+/g, '_').replace(/[^\w-]+/g, '');
+        return `page${categoria}.html?categoria=${categoria}/${tituloFormateado}`;
     }
 });
