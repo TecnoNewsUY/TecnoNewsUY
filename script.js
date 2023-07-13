2704c5d39f41ae789e8ffc26c2898af0acbb47f4
TecnoNewsUY <139405372+TecnoNewsUY@users.noreply.github.com>
2023-07-13 18:07:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,31 +5,33 @@ window.addEventListener('DOMContentLoaded', () => {
     // Obtener el contenedor del contenido de la noticia
     const noticiaContenido = document.getElementById('noticia-contenido');
 
-    // Obtener la categoría seleccionada de la URL
-    const urlParams = new URLSearchParams(window.location.search);
-    const categoriaSeleccionada = urlParams.get('categoria');
-
     // Cargar las noticias desde el archivo JSON
-    fetch('https://raw.githubusercontent.com/TecnoNewsUY/TecnoNewsUY/master/todaslasnoticias/todaslasnoticias.json')
+    fetch('https://raw.githubusercontent.com/TecnoNewsUY/TecnoNewsUY/master/todaslasnoticias/todaslasnoticias.json', { cache: 'no-store' })
         .then(response => response.json())
         .then(data => {
-            // Filtrar las noticias por categoría seleccionada
-            const noticiasFiltradas = data.filter(noticia => noticia.categoria === categoriaSeleccionada);
+            // Verificar si el archivo JSON contiene datos
+            if (data && data.length > 0) {
+                // Filtrar las noticias por categoría
+                const categoriaSeleccionada = obtenerCategoriaSeleccionada();
+                const noticiasFiltradas = data.filter(noticia => noticia.categoria === categoriaSeleccionada);
 
-            // Verificar si hay noticias en la categoría seleccionada
-            if (noticiasFiltradas.length > 0) {
-                // Recorrer las noticias filtradas y agregar opciones al selector
-                noticiasFiltradas.forEach((noticia, index) => {
-                    const option = document.createElement('option');
-                    option.value = index;
-                    option.textContent = noticia.titulo;
-                    noticiasSelector.appendChild(option);
-                });
+                // Verificar si hay noticias en la categoría seleccionada
+                if (noticiasFiltradas.length > 0) {
+                    // Recorrer las noticias filtradas y agregar opciones al selector
+                    noticiasFiltradas.forEach((noticia, index) => {
+                        const option = document.createElement('option');
+                        option.value = index;
+                        option.textContent = noticia.titulo;
+                        noticiasSelector.appendChild(option);
+                    });
 
-                // Mostrar la primera noticia por defecto
-                mostrarNoticia(noticiasFiltradas[0]);
+                    // Mostrar la primera noticia por defecto
+                    mostrarNoticia(noticiasFiltradas[0]);
+                } else {
+                    mostrarError("No se encontraron noticias en la categoría seleccionada.");
+                }
             } else {
-                mostrarError("No se encontraron noticias en la categoría seleccionada.");
+                mostrarError("No se encontraron noticias.");
             }
         })
         .catch(error => {
@@ -39,18 +41,24 @@ window.addEventListener('DOMContentLoaded', () => {
     // Cambiar el contenido de la noticia al seleccionar una opción del selector
     noticiasSelector.addEventListener('change', () => {
         const selectedIndex = noticiasSelector.value;
-        fetch('https://raw.githubusercontent.com/TecnoNewsUY/TecnoNewsUY/master/todaslasnoticias/todaslasnoticias.json')
+        fetch('https://raw.githubusercontent.com/TecnoNewsUY/TecnoNewsUY/master/todaslasnoticias/todaslasnoticias.json', { cache: 'no-store' })
             .then(response => response.json())
             .then(data => {
-                // Filtrar las noticias por categoría seleccionada
-                const noticiasFiltradas = data.filter(noticia => noticia.categoria === categoriaSeleccionada);
+                // Verificar si el archivo JSON contiene datos
+                if (data && data.length > 0) {
+                    // Filtrar las noticias por categoría
+                    const categoriaSeleccionada = obtenerCategoriaSeleccionada();
+                    const noticiasFiltradas = data.filter(noticia => noticia.categoria === categoriaSeleccionada);
 
-                // Verificar si hay noticias en la categoría seleccionada
-                if (noticiasFiltradas.length > 0) {
-                    const selectedNoticia = noticiasFiltradas[selectedIndex];
-                    mostrarNoticia(selectedNoticia);
+                    // Verificar si hay noticias en la categoría seleccionada
+                    if (noticiasFiltradas.length > 0) {
+                        const selectedNoticia = noticiasFiltradas[selectedIndex];
+                        mostrarNoticia(selectedNoticia);
+                    } else {
+                        mostrarError("No se encontraron noticias en la categoría seleccionada.");
+                    }
                 } else {
-                    mostrarError("No se encontraron noticias en la categoría seleccionada.");
+                    mostrarError("No se encontraron noticias.");
                 }
             })
             .catch(error => {
@@ -66,5 +74,11 @@ window.addEventListener('DOMContentLoaded', () => {
     // Función para mostrar un mensaje de error
     function mostrarError(mensaje) {
         noticiaContenido.innerHTML = `<p class="error">${mensaje}</p>`;
+    }
+
+    // Función para obtener la categoría seleccionada
+    function obtenerCategoriaSeleccionada() {
+        const urlParams = new URLSearchParams(window.location.search);
+        return urlParams.get('categoria');
     }
 });
