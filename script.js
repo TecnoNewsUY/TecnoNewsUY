d95ae3c04df94f81f346c50d3660b47bb2181ec3
TecnoNewsUY <139405372+TecnoNewsUY@users.noreply.github.com>
2023-07-13 16:04:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 window.addEventListener('DOMContentLoaded', () => {
     const categoria = '1'; // Categoría uno
 
-    fetch('https://raw.githubusercontent.com/TecnoNewsUY/TecnoNewsUY/master/todaslasnoticias/todaslasnoticias.json')
-        .then(response => response.json())
-        .then(data => {
-            const noticiasCategoria = data.filter(noticia => noticia.categoria.includes(categoria));
-            const noticiasMostradas = noticiasCategoria.slice(-3); // Mostrar las últimas 3 noticias
-
-            const noticiasContainer = document.getElementById('noticias-container');
-            noticiasContainer.innerHTML = ''; // Limpiar el contenedor de noticias
-
-            noticiasMostradas.forEach(noticia => {
-                const noticiaHTML = createNoticiaHTML(noticia.titulo, noticia.contenido);
-                noticiasContainer.innerHTML += noticiaHTML;
-            });
-        })
-        .catch(error => {
-            console.error('Error al obtener las noticias:', error);
-        });
+    const script = document.createElement('script');
+    script.src = `https://raw.githubusercontent.com/TecnoNewsUY/TecnoNewsUY/master/todaslasnoticias/todaslasnoticias.json?callback=procesarNoticias`;
+    document.body.appendChild(script);
 });
+
+function procesarNoticias(data) {
+    const noticiasCategoria = data.filter(noticia => noticia.categoria.includes(categoria));
+    const noticiasMostradas = noticiasCategoria.slice(-3); // Mostrar las últimas 3 noticias
+
+    const noticiasContainer = document.getElementById('noticias-container');
+    noticiasContainer.innerHTML = ''; // Limpiar el contenedor de noticias
+
+    noticiasMostradas.forEach(noticia => {
+        const noticiaHTML = createNoticiaHTML(noticia.titulo, noticia.contenido);
+        noticiasContainer.innerHTML += noticiaHTML;
+    });
+}
 
 function createNoticiaHTML(titulo, contenido) {
     const noticiaHTML = `
