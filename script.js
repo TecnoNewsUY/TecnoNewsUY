d326d0841b653124da72d6dff0614085449aae09
TecnoNewsUY <139405372+TecnoNewsUY@users.noreply.github.com>
2023-07-14 17:15:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
 fetch('https://raw.githubusercontent.com/TecnoNewsUY/TecnoNewsUY/master/todaslasnoticias/todaslasnoticias.json')
-  .then(response => response.json())
-  .then(data => {
-    const noticiasLista = document.getElementById('noticias-lista');
-    const noticiasPorCategoria = {};
+    .then(response => response.json())
+    .then(data => {
+        const noticiasLista = document.getElementById('noticias-lista');
+        const noticiasPorCategoria = {};
 
-    data.forEach(noticia => {
-      const categoria = noticia.categoria;
-      if (!noticiasPorCategoria.hasOwnProperty(categoria)) {
-        noticiasPorCategoria[categoria] = [];
-      }
-      noticiasPorCategoria[categoria].push(noticia);
-    });
+        data.forEach(noticia => {
+            const categoria = noticia.categoria;
+            if (!noticiasPorCategoria.hasOwnProperty(categoria)) {
+                noticiasPorCategoria[categoria] = [];
+            }
+            noticiasPorCategoria[categoria].push(noticia);
+        });
 
-    const noticias = [];
-    for (let categoria in noticiasPorCategoria) {
-      const noticiasCategoria = noticiasPorCategoria[categoria];
-      noticiasCategoria.forEach(noticia => {
-        if (noticias.length < 3 && !noticias.some(n => n.titulo === noticia.titulo)) {
-          noticias.push(noticia);
+        const noticias = [];
+        for (let categoria in noticiasPorCategoria) {
+            const noticiasCategoria = noticiasPorCategoria[categoria];
+            noticiasCategoria.forEach(noticia => {
+                if (noticias.length < 3 && !noticias.some(n => n.titulo === noticia.titulo)) {
+                    noticias.push(noticia);
+                }
+            });
         }
-      });
-    }
 
-    noticias.forEach(noticia => {
-      const divNoticia = document.createElement('div');
-      divNoticia.classList.add('noticia');
+        noticias.forEach(noticia => {
+            const divNoticia = document.createElement('div');
+            divNoticia.classList.add('noticia');
 
-      const titulo = document.createElement('h3');
-      titulo.textContent = noticia.titulo;
+            const titulo = document.createElement('h3');
+            titulo.textContent = noticia.titulo;
 
-      const contenido = document.createElement('p');
-      contenido.textContent = noticia.contenido;
+            const contenido = document.createElement('p');
+            contenido.textContent = noticia.contenido;
 
-      const imagen = document.createElement('img');
-      imagen.src = noticia.imagen;
-      imagen.alt = noticia.titulo;
+            const imagen = document.createElement('img');
+            imagen.src = noticia.imagen;
+            imagen.alt = noticia.titulo;
 
-      divNoticia.appendChild(titulo);
-      divNoticia.appendChild(contenido);
-      divNoticia.appendChild(imagen);
+            divNoticia.appendChild(titulo);
+            divNoticia.appendChild(contenido);
+            divNoticia.appendChild(imagen);
 
-      noticiasLista.appendChild(divNoticia);
-    });
-  })
-  .catch(error => console.log(error));
+            noticiasLista.appendChild(divNoticia);
+        });
+    })
+    .catch(error => console.log(error));
