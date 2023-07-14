6e414d87c260eedd881c50ca3a50cd160c1a3c88
TecnoNewsUY <139405372+TecnoNewsUY@users.noreply.github.com>
2023-07-14 18:30:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,134 +1,93 @@
 window.addEventListener('DOMContentLoaded', () => {
-  const noticiasLista = document.getElementById('noticias-lista');
+    const noticiasLista = document.getElementById('noticias-lista');
 
-  if (!noticiasLista) {
-    console.error("Error: No se encontró el elemento con el ID 'noticias-lista'");
-    return;
-  }
-
-  fetch('https://raw.githubusercontent.com/TecnoNewsUY/TecnoNewsUY/master/todaslasnoticias/todaslasnoticias.json')
-    .then(response => response.json())
-    .then(data => {
-      const noticiasPorCategoria = {};
-
-      data.forEach(noticia => {
-        const categoria = noticia.categoria;
-        if (!noticiasPorCategoria.hasOwnProperty(categoria)) {
-          noticiasPorCategoria[categoria] = [];
-        }
-        noticiasPorCategoria[categoria].push(noticia);
-      });
-
-      const noticias = obtenerUltimasNoticias(noticiasPorCategoria, 3);
-      mostrarNoticias(noticias, noticiasLista);
-    })
-    .catch(error => {
-      mostrarError("Error al cargar las noticias: " + error);
-    });
-
-  function obtenerUltimasNoticias(noticiasPorCategoria, cantidad) {
-    const noticiasUnicas = [];
-    const noticiasVistas = new Set();
-    let contador = 0;
-
-    for (let categoria in noticiasPorCategoria) {
-      const noticiasCategoria = noticiasPorCategoria[categoria];
-      for (let i = noticiasCategoria.length - 1; i >= 0; i--) {
-        const noticia = noticiasCategoria[i];
-        if (!noticiasVistas.has(noticia.titulo)) {
-          noticiasUnicas.push(noticia);
-          noticiasVistas.add(noticia.titulo);
-          contador++;
-        }
-
-        if (contador === cantidad) {
-          break;
-        }
-      }
-
-      if (contador === cantidad) {
-        break;
-      }
+    if (!noticiasLista) {
+        console.error("Error: No se encontró el elemento con el ID 'noticias-lista'");
+        return;
     }
 
-    return noticiasUnicas;
-  }
+    fetch('https://raw.githubusercontent.com/TecnoNewsUY/TecnoNewsUY/master/todaslasnoticias/todaslasnoticias.json')
+        .then(response => response.json())
+        .then(data => {
+            const noticiasPorCategoria = {};
 
-  function mostrarNoticias(noticias, contenedor) {
-    noticias.forEach(noticia => {
-      const noticiaElement = crearElementoNoticia(noticia);
-      contenedor.appendChild(noticiaElement);
-    });
-  }
+            data.forEach(noticia => {
+                const categoria = noticia.categoria;
+                if (!noticiasPorCategoria.hasOwnProperty(categoria)) {
+                    noticiasPorCategoria[categoria] = [];
+                }
+                noticiasPorCategoria[categoria].push(noticia);
+            });
 
-  function crearElementoNoticia(noticia) {
-    const noticiaElement = document.createElement('div');
-    noticiaElement.classList.add('noticia');
+            const noticias = obtenerUltimasNoticias(noticiasPorCategoria, 3);
+            mostrarNoticias(noticias, noticiasLista);
+        })
+        .catch(error => {
+            mostrarError("Error al cargar las noticias: " + error);
+        });
 
-    const imagenElement = document.createElement('img');
-    imagenElement.src = noticia.imagen;
-    imagenElement.alt = noticia.titulo;
-    imagenElement.classList.add('noticia-imagen');
+    function obtenerUltimasNoticias(noticiasPorCategoria, cantidad) {
+        const noticiasUnicas = [];
+        const noticiasVistas = new Set();
+        let contador = 0;
 
-    const contenidoElement = document.createElement('div');
-    contenidoElement.classList.add('noticia-contenido');
+        for (const categoria in noticiasPorCategoria) {
+            const noticiasCategoria = noticiasPorCategoria[categoria];
+            for (let i = noticiasCategoria.length - 1; i >= 0; i--) {
+                const noticia = noticiasCategoria[i];
+                if (!noticiasVistas.has(noticia.titulo)) {
+                    noticiasUnicas.push(noticia);
+                    noticiasVistas.add(noticia.titulo);
+                    contador++;
+                }
 
-    const tituloElement = document.createElement('h3');
-    tituloElement.textContent = noticia.titulo;
-    tituloElement.classList.add('noticia-titulo');
+                if (contador === cantidad) {
+                    break;
+                }
+            }
 
-    const cuerpoElement = document.createElement('p');
-    cuerpoElement.textContent = noticia.contenido.substring(0, 100) + '...';
+            if (contador === cantidad) {
+                break;
+            }
+        }
 
-    contenidoElement.appendChild(tituloElement);
-    contenidoElement.appendChild(cuerpoElement);
+        return noticiasUnicas;
+    }
 
-    noticiaElement.appendChild(imagenElement);
-    noticiaElement.appendChild(contenidoElement);
+    function mostrarNoticias(noticias, contenedor) {
+        contenedor.innerHTML = '';
 
-    noticiaElement.addEventListener('click', () => {
-      mostrarNoticiaCompleta(noticia);
-    });
+        noticias.forEach(noticia => {
+            const divNoticia = document.createElement('div');
+            divNoticia.classList.add('noticia');
 
-    return noticiaElement;
-  }
+            const titulo = document.createElement('h3');
+            const enlace = document.createElement('a'); // Nuevo elemento
+            enlace.href = noticia.enlace_noticia; // Nuevo código
+            enlace.textContent = noticia.titulo; // Nuevo código
+            titulo.appendChild(enlace); // Nuevo código
 
-  function mostrarNoticiaCompleta(noticia) {
-    const noticiaCompleta = document.createElement('div');
-    noticiaCompleta.classList.add('noticia-completa');
+            const contenido = document.createElement('p');
+            contenido.textContent = noticia.contenido;
 
-    const tituloElement = document.createElement('h2');
-    tituloElement.textContent = noticia.titulo;
+            const imagen = document.createElement('img');
+            imagen.src = noticia.imagen;
+            imagen.alt = noticia.titulo;
 
-    const contenidoElement = document.createElement('p');
-    contenidoElement.textContent = noticia.contenido;
+            divNoticia.appendChild(titulo);
+            divNoticia.appendChild(contenido);
+            divNoticia.appendChild(imagen);
 
-    noticiaCompleta.appendChild(tituloElement);
-    noticiaCompleta.appendChild(contenidoElement);
+            contenedor.appendChild(divNoticia);
+        });
+    }
 
-    noticiasLista.innerHTML = '';
-    noticiasLista.appendChild(noticiaCompleta);
-  }
+    function mostrarError(mensaje) {
+        const errorElement = document.createElement('p');
+        errorElement.classList.add('error');
+        errorElement.textContent = mensaje;
 
-  function mostrarError(mensaje) {
-    const errorElement = document.createElement('p');
-    errorElement.classList.add('error');
-    errorElement.textContent = mensaje;
-
-    noticiasLista.innerHTML = '';
-    noticiasLista.appendChild(errorElement);
-  }
+        noticiasLista.innerHTML = '';
+        noticiasLista.appendChild(errorElement);
+    }
 });
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(registration => {
-        console.log('Service Worker registrado con éxito:', registration.scope);
-      })
-      .catch(error => {
-        console.error('Error al registrar el Service Worker:', error);
-      });
-  });
-}
-
